c54a0fdde2a4a8b6e340d588dd63697c869870b3
Erin Grooms <eringrooms3746@gmail.com>
2019-04-11 11:44:38 -0500

    add files

--- Polygon.java
+++ Polygon.java
@@ -1,4 +1,14 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Polygon {
+public class Polygon extends PolyLine{
+	
+	public Polygon(Color color, boolean filled) {
+		
+	}
+	
+	public void draw(Graphics graphics) {
+		
+	}
 
 }
