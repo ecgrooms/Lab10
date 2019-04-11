c5cb21c1441198812edd0733e15997e94db7d78b
Erin Grooms <eringrooms3746@gmail.com>
2019-04-11 11:47:04 -0500

    edit files

--- PolyLine.java
+++ PolyLine.java
@@ -1,6 +1,13 @@
+import java.awt.Color;
+import java.awt.Point;
 
 public class PolyLine {
 	
+	//pointUL:Point, pointUR:Point,
+    //thickness:int, color:Color, fill:boolean
 	
+	public PolyLine(Point pointUL, Point pointUR, int thickness, Color color, boolean fill) {
+		
+	}
 
 }
