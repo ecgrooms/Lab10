197fd455e3f5c14525a79f229298c90e73d13e41
Erin Grooms <eringrooms3746@gmail.com>
2019-04-11 11:37:44 -0500

    add files

---
+++ PolyLine.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Point;
+
+public class PolyLine {
+	
+	protected Point[] location;
+	private Color color;
+	private boolean filled;
+	
+	public void Shape(Color color, boolean filled) {
+		this.color = color;
+		this.filled = filled;
+	}
+	
+	public Color getColor() {
+		
+	}
+	
+	public boolean isFilled() {
+		
+		
+	}
+	
+	public Point[] getLocation() {
+		
+	}
+
+}
